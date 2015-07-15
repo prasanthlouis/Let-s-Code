d2d33b8edfb4085361edb150adeb50436436aa9b
prasanthlouis <prasanthlouis21@gmail.com>
2015-07-15 10:15:29 +0530

    initial commit

---
+++ src/com/example/letscode/DisplayMessageActivity.java
@@ -0,0 +1,43 @@
+package com.example.letscode;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		//setContentView(R.layout.activity_display_message);
+		Intent intent=getIntent();
+		String message=intent.getStringExtra(MainActivity.sending_message);
+		TextView tv=new TextView(this);
+		tv.setTextSize(40);
+		tv.setText(message);
+		setContentView(tv);
+
+		
+	}
+
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	
+
+}
