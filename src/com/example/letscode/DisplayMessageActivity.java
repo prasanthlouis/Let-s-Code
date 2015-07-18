cc39de53525c8f5e2e50aca9ff7f3727413eebd1
prasanthlouis <prasanthlouis21@gmail.com>
2015-07-18 18:27:10 +0530

    Different Devices Commit

--- src/com/example/letscode/DisplayMessageActivity.java
+++ src/com/example/letscode/DisplayMessageActivity.java
@@ -1,20 +1,21 @@
 package com.example.letscode;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class DisplayMessageActivity extends ActionBarActivity {
+public class DisplayMessageActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//setContentView(R.layout.activity_display_message);
+	//	setContentView(R.layout.activity_display_message);
 		getActionBar().setDisplayHomeAsUpEnabled(true);
 		Intent intent=getIntent();
 		String message=intent.getStringExtra(MainActivity.sending_message);
+	//	String message=getResources().getString(R.string.app_name);
 		TextView tv=new TextView(this);
 		tv.setTextSize(40);
 		tv.setText(message);
--- src/com/example/letscode/MainActivity.java
+++ src/com/example/letscode/MainActivity.java
@@ -1,23 +1,35 @@
 package com.example.letscode;
 
+import android.app.ActionBar;
+import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 public final static String sending_message="com.example.letscode.message";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		setUpActionBar();
 		
 	}
+	
+	
+private void setUpActionBar() {
+    // Make sure we're running on Honeycomb or higher to use ActionBar APIs
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+        ActionBar actionBar = getActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
